74cf273702bd7720fca80be5162746746094dce9
ochoaFranco <ochoa.franco132@gmail.com>
2023-09-11 19:53:20 -0300

    uploading changes

--- generateJungle.ts
+++ generateJungle.ts
@@ -1,6 +1,7 @@
 import { PLAYER, TRAPS, TREASURES } from "./Types"
-
+// TODO generateLines with no more than 2 traps per row or column.
 export class GenerateJungle {
+  private static index: number[] = [0, 0, 0, 0, 0, 0, 0];
   public static generateDefault () {
     const table: number[][] = []
     for (let i = 0;i < 7;i++) {
@@ -10,18 +11,19 @@ export class GenerateJungle {
   }
 
   private static generateLine (trap: boolean) {
-    let indexTrap
+    let indexTrap = -1
     if (trap) {
       indexTrap = Math.floor(Math.random() * 5) + 1
     }
     const line: number[] = []
     for (let i = 0;i < 7;i++) {
-      if (trap && indexTrap === i) {
+      if (trap && i > 0 && i < 6 && this.index[i] < 2) {
         line.push(TRAPS[0])
+        this.index[i]++
       } else if (!trap && i === 3) {
         line.push(PLAYER)        
       } else {
-        line.push(TREASURES[Math.floor(Math.random() * 3)])
+        line.push(TREASURES[Math.floor(Math.random() * 3)]) // selecting a treasure to push.
       }
     }
     return line
