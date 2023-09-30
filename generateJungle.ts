01052a6597a1c8728471c8a6a7fc01b78f4f8fdd
ochoaFranco <ochoa.franco132@gmail.com>
2023-09-30 07:18:57 -0300

    fixing scorpion functionality

--- generateJungle.ts
+++ generateJungle.ts
@@ -1,6 +1,6 @@
 import { Coordinate } from "./Coordinate";
 import { PLAYER, SCORPION, SPIKE, TREASURES } from "./Types"
-// TODO generateLines with no more than 2 traps per row or column.
+
 export class GenerateJungle {
   public static generateDefault () {
     const table: number[][] = [] /*[  
@@ -30,8 +30,8 @@ export class GenerateJungle {
       if (i < 50) {
         gemsPool.push(TREASURES[Math.floor(Math.random() * TREASURES.length)])
       } else {
-        const indexRandom = TREASURES[Math.floor(Math.random() * (TREASURES.length + 1))] // generate scorpion
-        if (indexRandom > TREASURES.length) {
+        const indexRandom = Math.floor(Math.random() * (TREASURES.length + 1))
+        if (indexRandom >= TREASURES.length) {
           gemsPool.push(SCORPION[Math.floor(Math.random() * SCORPION.length)])
         } else {
           gemsPool.push(TREASURES[indexRandom])
